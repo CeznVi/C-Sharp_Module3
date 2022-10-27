e584d395cbe4db8125f0eee1e9189f50ef69c233
Viktor <Cezn.Vi@gmail.com>
2022-10-27 21:57:11 +0300

    Добавить README.md.

---
+++ C-Sharp_Module3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
